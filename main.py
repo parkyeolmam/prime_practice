5c92c6aae3ec91fce4ec0b450a3e35eed9685cc8
parkyeolmam <48434523+parkyeolmam@users.noreply.github.com>
2019-03-12 02:37:48 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+def start():
+    keep_going = True
+    while keep_going:
+        n = input("Please select which prime algorithm to use, or type 'q' to quit.\n>>> ")
+        print() #newline
+        try:
+            int(n)
+        except ValueError:
+            if n == "quit" or n == "q":
+                print("Bye!")
+                break
+            else:
+                print("Input not recognized, please try again.\n")
+                continue
+        exec("prime" + str(n) + ".get()")
+
+
+number_of_algorithms = 1 #manually update
+for i in range(1, number_of_algorithms + 1):
+    exec("import prime" + str(number_of_algorithms))
+    
+start()
